abed70be31dd20054f1f1cb7e90f9fb406e75db8
Fidel_Yiu <fidel-yiu@qq.com>
2021-07-04 15:23:58 +0800

    笔记ws的交互实体修改

--- model/vo/note-read-vo.go
+++ model/vo/note-read-vo.go
@@ -4,6 +4,6 @@ import "yiu/yiu-reader/model/enum"
 
 // NoteReadVo 读取本文件时，结束的VO
 type NoteReadVo struct {
-	Path   string              // 路径
-	Result enum.NoteReadResult // 读取结果
+	Path   string              `json:"path"`   // 路径
+	Result enum.NoteReadResult `json:"result"` // 读取结果
 }
